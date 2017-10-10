379a3baac1332af20900f68d0955528dbd50ccbb
ssrinivass <32629751+ssrinivass@users.noreply.github.com>
2017-10-10 11:21:20 +0530

    Srinivas

---
+++ consumer.js
@@ -0,0 +1,18 @@
+﻿function charges(){
+    var units= document.getElementById("nunits").value;
+var charge;
+var total;
+if (units >0 && units <=100)
+{
+    charge = (296 / 100);
+    total = (units * charge);
+}
+else if (units > 100)
+{
+    charge = (556 / 100);
+    total = (units * charge);
+}
+window.alert("No of units are : " + units.toString() + "." +
+    "Charges per unit : " + charge + "." +
+    "The Electricity Charges are :" + total + ".");
+}
